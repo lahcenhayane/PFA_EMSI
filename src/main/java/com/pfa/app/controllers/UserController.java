2463fb939c0cf3cc131612e7a2cb0020eb7be278
Lahcen HAYANE <Etu_HAYANE.Lahcen@emsi-edu.ma>
2021-06-25 12:17:00 +0100

    BY HAYANE Lahcen

--- src/main/java/com/pfa/app/controllers/UserController.java
+++ src/main/java/com/pfa/app/controllers/UserController.java
@@ -3,6 +3,8 @@ package com.pfa.app.controllers;
 import com.pfa.app.services.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
@@ -10,6 +12,9 @@ public class UserController {
     @Autowired
     IUserService userService;
 
-
+    @PostMapping(value = {"/login"})
+    public String login(Model model){
+        return "login";
+    }
 
 }
